57170684feb50569bf8f7195c928eabb07b324c0
Christian Ing <cisunardi1@sheffield.ac.uk>
2018-05-01 10:57:34 +0700

    Added redux-logger

--- store.js
+++ store.js
@@ -1,8 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'; 
 
-const enhancer = compose(applyMiddleware(thunk));
+// checking to see if it's on DEV mode
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__});
+
+const enhancer = compose(
+  applyMiddleware(
+    thunk,
+    loggerMiddleware,
+  )
+);
 
 const store = createStore(rootReducer,enhancer);
 
